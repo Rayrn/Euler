93a46218bd7d58f4728dd0fc458263c4ffec2a25
Jack Hansard <jack.hansard@immediate.co.uk>
2016-12-02 15:28:04 +0000

    Base Files

---
+++ www/controllers/problems/15.php
@@ -0,0 +1,33 @@
+<?php
+/* Registration Form Processing */
+if(!defined('APP_ROOT')) {
+    exit('No direct script access allowed');
+}
+
+// Check if the user is allowed to access this page
+if(!$auth_user) {
+    header("Location: /");
+    exit();
+}
+
+//------------------------------------------------------------------
+// Action processing
+//------------------------------------------------------------------
+// Fetch Action
+$action = isset($_GET['action']) ? $_GET['action'] : '';
+$action = isset($_POST['action']) ? $_POST['action'] : $action;
+
+
+//------------------------------------------------------------------
+// Problem processing
+//------------------------------------------------------------------
+/** Problem details:
+ * Which starting number, under one million, produces the longest chain?
+ */
+
+if($action == '') {
+
+
+    // Display form
+    require_once (VIEW_ROOT.'/problems/15.php');
+}
---
+++ www/views/problems/15.php
@@ -0,0 +1,34 @@
+<?php
+/* Login Form Display */
+if(!defined('APP_ROOT')) {
+    exit('No direct script access allowed');
+}
+
+// Load page head
+require_once APP_ROOT.'/inc/page_begin.php';
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="panel-heading">
+               <div class="panel-title text-center">
+                    <h1 class="title text-warning">Lattice Paths</h1>
+                    <hr />
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6 col-md-offset-3">
+            <div class="row ">
+                <div class="col-xs-12">
+                        
+                </div><!-- /.col-xs-12 -->
+            </div><!-- /.row -->
+        </div><!-- /.col-md-6 col-md-offset-3 -->
+    </div><!-- /.row -->
+</div>
+
+<?php
+// Load page end
+require_once APP_ROOT.'/inc/page_end.php';
