a7432a9a9389323108837fa0473082ddb27836cc
Jack Hansard <jack.hansard@immediate.co.uk>
2016-12-16 12:21:34 +0000

    Problem 22 (and typo fix)

--- www/views/problems/22.php
+++ www/views/problems/22.php
@@ -13,7 +13,7 @@ require_once APP_ROOT.'/inc/page_begin.php';
         <div class="col-xs-12">
             <div class="panel-heading">
                <div class="panel-title text-center">
-                    <h1 class="title text-warning">Amicable numbers</h1>
+                    <h1 class="title text-warning">Names scores</h1>
                     <hr />
                 </div>
             </div>
@@ -22,7 +22,7 @@ require_once APP_ROOT.'/inc/page_begin.php';
         <div class="col-md-6 col-md-offset-3">
             <div class="row ">
                 <div class="col-xs-12 text-center">
-                    Evaluate the sum of all the amicable numbers under 10000?
+                    What is the total of all the name scores in the file?
                 </div><!-- /.col-xs-12 -->
                 <div class="col-xs-12 text-center">
                      <?php echo number_format($answer); ?>
